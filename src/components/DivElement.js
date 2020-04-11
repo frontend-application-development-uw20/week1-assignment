379d77a76df9b735a29bd6e5d571348956a0b515
Sven Otto <svenotto@uw.edu>
2020-04-11 10:29:39 -0700

    week 1 homework

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,17 @@
 // Import HTMLElement here
+import HTMLElement from './HTMLElement';
 
 // Define class here
+class DivElement extends HTMLElement {
+	constructor (content) {
+		super(content);
+		this.content = content;
+		this.tag = 'div';
+	}
+}
 
 // Export class here
+export default DivElement;
+
+// const andIThinkToMyself = new DivElement('What a wonderful world');
+// console.log(andIThinkToMyself.render());
--- src/rolodex/rolodexPrinter.js
+++ src/rolodex/rolodexPrinter.js
@@ -1,11 +1,23 @@
+// Old Code
+// import people from './people.json';
+
+// people.forEach(function (person) {
+//   const names = person.name.split(' ');
+//   const firstName = names[0];
+//   const lastName = names[1];
+//   const email = person.email;
+//   const phone = person.phone;
+
+//   console.log('First name: ' + firstName + '\nLast name: ' + lastName + '\nEmail: ' + email + '\nPhone number: ' + phone + '\n');
+// });
+
+// New Code
 import people from './people.json';
 
 people.forEach(function (person) {
-  const names = person.name.split(' ');
-  const firstName = names[0];
-  const lastName = names[1];
-  const email = person.email;
-  const phone = person.phone;
+	const names = person.name.split(' ');
+	const [firstName, lastName] = [names[0], names[1]];
+	const { email, phone } = person;
 
-  console.log('First name: ' + firstName + '\nLast name: ' + lastName + '\nEmail: ' + email + '\nPhone number: ' + phone + '\n');
+  console.log(`First name: ${firstName} Last name: ${lastName} Email: ${email} Phone number: ${phone}`);
 });
