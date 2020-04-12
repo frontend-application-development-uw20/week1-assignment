4f623e21c31357ca77e70062c248dc13328e123c
Fletcher Bonds <fbonds@uw.edu>
2020-04-12 15:35:11 -0700

    saving work in progress

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,10 @@
 // Import HTMLElement here
-
+import * as HTMLElement from './HTMLElement';
 // Define class here
-
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super('div', content);
+  }
+}
 // Export class here
+export default DivElement;
