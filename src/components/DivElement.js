00400c80ba1abe2035e7d61847406f147f791f3d
Fletcher Bonds <fbonds@uw.edu>
2020-04-12 17:21:08 -0700

    homework complete

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,10 +1,13 @@
 // Import HTMLElement here
-import * as HTMLElement from './HTMLElement';
+import HTMLElement from './HTMLElement';
 // Define class here
 class DivElement extends HTMLElement {
   constructor(content) {
-    super('div', content);
+    super(content);
+    this.tag = 'div';
+    this.content = content;
   }
 }
+
 // Export class here
 export default DivElement;
--- src/components/HTMLElement.js
+++ src/components/HTMLElement.js
@@ -11,4 +11,4 @@ class HTMLElement {
 }
 
 // Export class here
-export default { HTMLElement };
+export default HTMLElement;
