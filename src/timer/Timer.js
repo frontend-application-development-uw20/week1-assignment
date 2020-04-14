fa46b57e40c2040bb44f4e0194b07a81ea47130a
Andrew Weiss <wandrew8@gmail.com>
2020-04-14 08:16:41 -0700

    answers added

--- src/timer/Timer.js
+++ src/timer/Timer.js
@@ -2,15 +2,13 @@ function Timer(seconds) {
   this.seconds = seconds;
 }
 
-Timer.prototype.start = function () {
-  var instance = this;
-  var timerInterval = setInterval(function () {
-    if (instance.seconds === 0) {
+Timer.prototype.start = () => {
+  const timerInterval = setInterval(() => {
+    if (this.seconds === 0) {
       clearInterval(timerInterval);
     }
 
-    console.log(instance.seconds);
-    instance.seconds -= 1;
+    this.seconds -= 1;
   }, 1000);
 };
 
