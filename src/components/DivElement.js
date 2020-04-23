8738bd4c8c0b67cd8bbbc1889a00dc7ddc2c0462
Rolly Seth <rseth@uw.edu>
2020-04-22 18:28:05 -0700

    Completed HW

--- src/components/DivElement.js
+++ src/components/DivElement.js
@@ -1,5 +1,15 @@
 // Import HTMLElement here
-
+// Import HTMLElement here
+import HTMLElement from './HTMLElement';
 // Define class here
-
+class DivElement extends HTMLElement {
+  constructor(content) {
+    super('div', content);
+  }
+}
+const andIThinkToMyself = new DivElement('What a wonderful world');
+// andIThinkToMyself.render();
+console.log(andIThinkToMyself.render());
+// prints "<div>What a wonderful world</div>"
 // Export class here
+export default DivElement;
